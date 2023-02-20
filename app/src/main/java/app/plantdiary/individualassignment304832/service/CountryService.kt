35241878c4ab12d67f697c32a3b460e45cd7ec2b
bonima21 <79886034+bonima21@users.noreply.github.com>
2023-02-19 20:20:10 -0500

    Create DTO

--- app/src/main/java/app/plantdiary/individualassignment304832/service/CountryService.kt
+++ app/src/main/java/app/plantdiary/individualassignment304832/service/CountryService.kt
@@ -8,8 +8,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import retrofit2.awaitResponse
 
-class CountryService {
-
+class CountryService{
     suspend fun fetchCountries(): List<Country>? {
         return withContext(Dispatchers.IO) {
             val retrofit = RetrofitClientInstance.retrofitInstance?.create(ICountryDAO::class.java)
